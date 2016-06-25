73af5861d56f6a3bd7c5fc07b90df5b4bc63bdbf
Ubuntu <ubuntu@ip-172-31-25-18.eu-central-1.compute.internal>
2016-06-25 08:05:09 +0000

    initial commit

--- server/startrabbit.sh
+++ server/startrabbit.sh
@@ -1,25 +1,53 @@
-#!/bin/bash
-
 if [ -z "$CLUSTERED" ]; then
-	# if not clustered then start it normally as if it is a single server
-	/usr/sbin/rabbitmq-server	
+# Wenn nicht geclustered werden soll, wird einfach ein normaler RabbitMQ Server gestartet
+	( sleep 10 ; \
+
+	# Virtuellen Host erstellen
+	rabbitmqctl add_vhost ${SERVICE}host ; \
+
+	# Erstellen der Nutzer und Berechtigungen
+	rabbitmqctl add_user ${SERVICE} ${SERVICE} ; \
+	rabbitmqctl set_user_tags ${SERVICE} ${SERVICE}service ; \
+	rabbitmqctl set_permissions -p ${SERVICE}host ${SERVICE} ".*" ".*" ".*" ; \
+	rabbitmqctl set_permissions -p ${SERVICE}host guest ".*" ".*" ".*" ; \
+	
+	#Federation eisntellen
+	rabbitmqctl set_parameter -p ${SERVICE}host federation-upstream booking-upstream '{"uri":"amqp://bookingroot:5672","expires":3600000}' ; \
+	rabbitmqctl set_parameter -p ${SERVICE}host federation-upstream customermanagement-upstream '{"uri":"amqp://customermanagementroot:5672","expires":3600000}' ; \
+	rabbitmqctl set_parameter -p ${SERVICE}host federation-upstream pricing-upstream '{"uri":"amqp://pricingroot:5672","expires":3600000}' ; \
+	rabbitmqctl set_parameter -p ${SERVICE}host federation-upstream seatmanagement-upstream '{"uri":"amqp://seatmanagementroot:5672","expires":3600000}' ; \
+	rabbitmqctl set_parameter -p ${SERVICE}host federation-upstream seatoverview-upstream '{"uri":"amqp://seatoverviewroot:5672","expires":3600000}' ; \
+
+
+	rabbitmqctl set_policy -p ${SERVICE}host --apply-to exchanges federate-all ".*" '{"federation-upstream-set":"all"}' ; \
+
+	# Regel für Cluster
+	rabbitmqctl set_policy -p ${SERVICE}host booking ".*" '{"ha-mode":"all", "ha-sync-mode":"automatic"}' ; \
+	) &
+
+	#Server starten
+	/usr/sbin/rabbitmq-server $@
+
 else
+
 	if [ -z "$CLUSTER_WITH" ]; then
-		# If clustered, but cluster with is not specified then again start normally, could be the first server in the
-		# cluster
+		# Wenn geclustered werden soll, aber kein Einstiegspunkt gegeben ist, dann wird ein normaler Server gestartet
 		/usr/sbin/rabbitmq-server
 	else
+
+		#Ansonsten wird der Server gestartet und gestopped, um den Befehl zum Clustern auszufuehren 
 		/usr/sbin/rabbitmq-server -detached
 		rabbitmqctl stop_app
+
+		# Festlegung, ob der Knoten auf dem Arbeitsspeicher oder auf die Festplatte schreiben soll
 		if [ -z "$RAM_NODE" ]; then
 			rabbitmqctl join_cluster rabbit@$CLUSTER_WITH
 		else
 			rabbitmqctl join_cluster --ram rabbit@$CLUSTER_WITH
 		fi
-		rabbitmqctl start_app
 
-		# Tail to keep the a foreground process active..
+		# Korrektes Starten und Logging des Servers
+		rabbitmqctl start_app
 		tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 	fi
 fi
-
